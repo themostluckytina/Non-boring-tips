ef559926629f67adb61527b141d5dcf6e0e9f56f
themostluckytina <themostluckytina@gmail.com>
2022-05-03 22:55:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+  const [tip, setTip] = useState('');
+
+  const getTip =async()=>{
+    const response = await fetch("http://www.boredapi.com/api/activity/");
+    const data = await response.json();
+    setTip(data.activity)
+    console.log(data.activity)
 }
 
+useEffect(()=>{
+  getTip();
+}, [])
+
+
+
+
+  return( <div className="center">
+    <div >
+      <h1>stop being boring & start any activity!</h1>
+    
+    </div>
+    
+    <div>
+      <h3>{tip}</h3>
+    </div>
+    <div>
+      <button onClick={getTip}>Next tip</button>
+    </div>
+    <div>
+      <img src="https://nomadnarrativeorg.files.wordpress.com/2018/09/lazy-woman-e1535904728112.jpg?w=1000" alt="cc"/>
+    </div>
+
+
+</div>
+  )
+}
 export default App;
